306422626aa4a277986b208a52644617a6a79381
vvcrxyz <nomar.limon247933@potros.itson.edu.mx>
2026-02-28 15:58:51 -0700

    Agregué componente PokemonNeighbor y ajustes visuales

---
+++ app/src/main/java/components/Chip.kt
@@ -0,0 +1,28 @@
+package components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import limon.nomar.composepokedex_LimonNomar.ui.theme.ElectricYellow
+import limon.nomar.composepokedex_LimonNomar.ui.theme.White
+
+@Composable
+fun Chip(text: String, color: Color, modifier: Modifier = Modifier){
+    Box(modifier.padding(5.dp, 2.dp)
+        .background(ElectricYellow, RoundedCornerShape(16.dp))){
+        Text(text)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ChipPreview(){
+    Chip("Electrico", ElectricYellow)
+}
